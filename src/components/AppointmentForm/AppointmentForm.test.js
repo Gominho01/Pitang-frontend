28091c4921af8d50019b0cec7909a5ae52b63e47
LucasGominho <lcpg@cesar.school>
2024-07-13 18:07:15 -0300

    test: should fail to submit appointment

--- src/components/AppointmentForm/AppointmentForm.test.js
+++ src/components/AppointmentForm/AppointmentForm.test.js
@@ -47,5 +47,24 @@ describe('<AppointmentForm/>', () => {
       expect(inputBirthDate.value).toBe('');
       expect(inputAppointmentDay.value).toBe('');
     });
+    const messageSuccess = await screen.findByText(/Agendamento Criado com Sucesso/i)
+    expect(messageSuccess).toBeInTheDocument();
+  });
+  
+  it('should fail to submit appointment', async () => {
+    axios.post.mockRejectedValue(new Error('Erro ao Criar Agendamento'));
+    customRender(<AppointmentForm />);
+
+    const inputName = screen.getByLabelText('Nome:');
+    const inputBirthDate = screen.getByLabelText('Data de Nascimento:');
+    const inputAppointmentDate = screen.getByLabelText('Data e Hora do Agendamento:');
+    const submitButton = screen.getByRole('button', { name: /Agendar/i });
+
+    fillAndSubmitForm(inputName, inputBirthDate, inputAppointmentDate, submitButton);
+    fillAndSubmitForm(inputName, inputBirthDate, inputAppointmentDate, submitButton);
+    fillAndSubmitForm(inputName, inputBirthDate, inputAppointmentDate, submitButton);
+
+    const messageError = await screen.findByText(/Erro ao Criar Agendamento/i);
+    expect(messageError).toBeInTheDocument();
   });
 });
