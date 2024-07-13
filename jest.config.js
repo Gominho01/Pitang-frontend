3b15248be67ecd4fd6f004dba8a437ab424dce98
LucasGominho <lcpg@cesar.school>
2024-07-13 20:24:44 -0300

    chore: initial TypeScript configuration

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    testEnvironment: 'jsdom',
-    moduleNameMapper: {
-      "^.+\\.svg$": "jest-svg-transformer",
-      "^.+\\.(css|less|scss)$": "identity-obj-proxy"
-    },
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx']
-  };
---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
+
+export default config;
