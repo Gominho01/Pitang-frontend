58af0792e1ffe2fa61bb158ebf127a750b4285ac
LucasGominho <lcpg@cesar.school>
2024-07-13 19:27:20 -0300

    test: renders heading

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
 import Navbar from './Navbar';
 import '@testing-library/react';
 
+const renderWithRouter = (ui, { route = '/' } = {}) => {
+  window.history.pushState({}, 'Test page', route);
+
+  return render(
+    <MemoryRouter initialEntries={[route]}>
+      <Routes>
+        <Route path="*" element={ui} />
+      </Routes>
+    </MemoryRouter>
+  );
+};
 
 describe('Navbar', () => {
+  it('should render the correct heading', () => {
+    renderWithRouter(<Navbar />);
 
+    expect(screen.getByText('Vaccine Scheduler')).toBeInTheDocument();
+  });
 });
