b993182621bda0361fcef41d15038afe02b000e9
LucasGominho <lcpg@cesar.school>
2024-07-13 17:40:54 -0300

    test: should handle forms error

--- src/components/AppointmentForm/AppointmentForm.test.js
+++ src/components/AppointmentForm/AppointmentForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {screen} from '@testing-library/react';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
 import AppointmentForm from './AppointmentForm';
 import axios from 'axios';
 import { customRender } from '../../utils/customRender';
@@ -10,5 +10,19 @@ describe('<AppointmentForm/>', () => {
   it('should render correctly and show title', async () => {
     customRender(<AppointmentForm />);
     expect(screen.getByRole('heading', { name: /Agendamento/i })).toBeInTheDocument();
+  });
+
+  describe('<AppointmentForm/>', () => {
+    it('should handle forms error', async () => {
+      customRender(<AppointmentForm />);
+
+      const submitButton = screen.getByRole('button', { name: /Agendar/i });
+      fireEvent.click(submitButton);
+      await waitFor(() => {
+        expect(screen.getByTestId('name-error')).toHaveTextContent('Nome é obrigatório');
+        expect(screen.getByTestId('birthDate-error')).toHaveTextContent('Required');
+        expect(screen.getByTestId('appointmentDay-error')).toHaveTextContent('Required');
+      });
+    });
   });
 });
