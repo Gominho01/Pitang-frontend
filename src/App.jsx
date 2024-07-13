3cf526f78b829486c36af3a0ed1f4e434294f7a7
LucasGominho <lcpg@cesar.school>
2024-07-13 15:21:11 -0300

    feat: using the context on app

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import AppointmentPage from './components/AppointmentForm';
+import { ModalProvider } from './context/modalContext';
+import CustomModal from './utils/customModal';
 
 function App() {
   return (
     <ChakraProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<AppointmentPage />} />
-        </Routes>
-      </Router>
+      <ModalProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<AppointmentPage />} />
+          </Routes>
+          <CustomModal />
+        </Router>
+      </ModalProvider>
     </ChakraProvider>
   );
 }
