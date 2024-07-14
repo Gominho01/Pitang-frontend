4987be62f85933d2d1c1f1c8da0a1403eeb819af
LucasGominho <lcpg@cesar.school>
2024-07-13 21:01:42 -0300

    chore: finishing test configuration setup

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
+    '^.+\\.css$': 'identity-obj-proxy',
     '^@/(.*)$': '<rootDir>/src/$1'
   },
   transform: {
@@ -11,6 +12,8 @@ const config: Config.InitialOptions = {
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
 };
 
 export default config;
