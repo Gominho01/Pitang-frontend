5133d4864d4c04a5d9f3eb9bb56b7abdf043acaa
LucasGominho <lcpg@cesar.school>
2024-07-15 17:55:56 -0300

    refactor: moving AppointmentList functions to utils

--- src/pages/AppointmentListPage.tsx
+++ src/pages/AppointmentListPage.tsx
@@ -3,7 +3,7 @@ import { Box, Heading, Text, Select } from '@chakra-ui/react';
 import { Appointment, GroupedAppointments } from '../interfaces/List.interfaces';
 import AppointmentGroup from '../components/AppointmentList/AppointmentGroup';
 import api from '../services/api';
-import { getAllDatesWithAppointments } from '../utils/appointmentsUtils';
+import { getAllDatesWithAppointments, groupedAppointmentsByDateTime } from '../utils/appointmentsUtils';
 
 const AppointmentsPage: React.FC = () => {
   const [appointments, setAppointments] = useState<Appointment[]>([]);
@@ -39,55 +39,8 @@ const AppointmentsPage: React.FC = () => {
     }
   };
 
-  const isValidDate = (date: Date): boolean => {
-    return date instanceof Date && !isNaN(date.getTime());
-  };
-
-  const groupedAppointmentsByDateTime = (): GroupedAppointments => {
-    const groupedAppointments: GroupedAppointments = {};
-
-    const sortedAppointments = [...appointments].sort((a, b) => {
-      const dateA = new Date(a.appointmentDate);
-      const dateB = new Date(b.appointmentDate);
-      return dateA.getTime() - dateB.getTime();
-    });
-
-    sortedAppointments.forEach((appointment) => {
-      const appointmentDate = new Date(appointment.appointmentDate);
-      if (!isValidDate(appointmentDate)) {
-        console.warn(`Invalid date found: ${appointment.appointmentDate}`);
-        return;
-      }
-
-      const day = appointmentDate.toLocaleDateString();
-      const time = appointmentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-
-      if (selectedDate && day !== selectedDate) {
-        return;
-      }
-
-      if (!groupedAppointments[day]) {
-        groupedAppointments[day] = {};
-      }
-
-      if (!groupedAppointments[day][time]) {
-        groupedAppointments[day][time] = [];
-      }
-
-      groupedAppointments[day][time].push(appointment);
-    });
-
-    return groupedAppointments;
-  };
-
-  const groupedAppointments = groupedAppointmentsByDateTime();
-  let allDatesWithAppointments = getAllDatesWithAppointments(appointments);
-
-  allDatesWithAppointments = allDatesWithAppointments.sort((a, b) => {
-    const dateA = new Date(a.split('/').reverse().join('-'));
-    const dateB = new Date(b.split('/').reverse().join('-')); 
-    return dateA.getTime() - dateB.getTime();
-  });
+  const groupedAppointments = groupedAppointmentsByDateTime(appointments, selectedDate);
+  const allDatesWithAppointments = getAllDatesWithAppointments(appointments);
 
   return (
     <Box maxW="600px" mx="auto" mt={20} p={8} borderWidth={1} borderRadius="md" boxShadow="md" bg="gray.50" color="teal.600" borderColor="teal.400">
--- src/utils/appointmentsUtils.ts
+++ src/utils/appointmentsUtils.ts
@@ -1,4 +1,4 @@
-import { Appointment } from '../interfaces/List.interfaces';
+import { Appointment, GroupedAppointments } from '../interfaces/List.interfaces';
 
 export const getAllDatesWithAppointments = (appointments: Appointment[]): string[] => {
   const datesSet = new Set<string>();
@@ -8,9 +8,53 @@ export const getAllDatesWithAppointments = (appointments: Appointment[]): string
       datesSet.add(appointmentDate.toLocaleDateString());
     }
   });
-  return Array.from(datesSet);
+
+  const sortedDates = Array.from(datesSet).sort((a, b) => {
+    const dateA = new Date(a.split('/').reverse().join('-'));
+    const dateB = new Date(b.split('/').reverse().join('-')); 
+    return dateA.getTime() - dateB.getTime();
+  });
+
+  return sortedDates;
 };
 
-const isValidDate = (date: Date): boolean => {
+export const isValidDate = (date: Date): boolean => {
   return date instanceof Date && !isNaN(date.getTime());
+};
+
+export const groupedAppointmentsByDateTime = (appointments: Appointment[], selectedDate: string | null): GroupedAppointments => {
+  const groupedAppointments: GroupedAppointments = {};
+
+  const sortedAppointments = [...appointments].sort((a, b) => {
+    const dateA = new Date(a.appointmentDate);
+    const dateB = new Date(b.appointmentDate);
+    return dateA.getTime() - dateB.getTime();
+  });
+
+  sortedAppointments.forEach((appointment) => {
+    const appointmentDate = new Date(appointment.appointmentDate);
+    if (!isValidDate(appointmentDate)) {
+      console.warn(`Invalid date found: ${appointment.appointmentDate}`);
+      return;
+    }
+
+    const day = appointmentDate.toLocaleDateString();
+    const time = appointmentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+    if (selectedDate && day !== selectedDate) {
+      return;
+    }
+
+    if (!groupedAppointments[day]) {
+      groupedAppointments[day] = {};
+    }
+
+    if (!groupedAppointments[day][time]) {
+      groupedAppointments[day][time] = [];
+    }
+
+    groupedAppointments[day][time].push(appointment);
+  });
+
+  return groupedAppointments;
 };
