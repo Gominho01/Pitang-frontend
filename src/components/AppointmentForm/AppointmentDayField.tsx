0050be9ba65c9b2713fcbdbf72a7b9b5fb08f66f
LucasGominho <lcpg@cesar.school>
2024-07-17 16:51:38 -0300

    feat: limiting mintime based on hour

--- src/components/AppointmentForm/AppointmentDayField.tsx
+++ src/components/AppointmentForm/AppointmentDayField.tsx
@@ -4,9 +4,27 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { AppointmentDayFieldProps } from '../../interfaces/Forms.interfaces';
 import { removeMilliseconds } from '../../utils/appointmentsUtils';
+import dayjs from 'dayjs';
 
 const AppointmentDayField: React.FC<AppointmentDayFieldProps> = ({ setValue, watch, errors }) => {
   const watchAppointmentDay = watch('appointmentDay');
+
+  const isDisabledTime = (time: Date) => {
+    const currentHour = dayjs().hour();
+    const selectedHour = dayjs(time).hour();
+    const currentMinute = dayjs().minute();
+    const selectedMinute = dayjs(time).minute();
+
+    if (dayjs().isSame(watchAppointmentDay, 'day') && selectedHour === currentHour && selectedMinute <= currentMinute) {
+      return false;
+    }
+
+    return selectedHour < 11 || selectedHour > 20 || (dayjs().isSame(watchAppointmentDay, 'day') && selectedHour < currentHour);
+  };
+
+  const filterTime = (time: Date) => {
+    return !isDisabledTime(time);
+  };
 
   return (
     <FormControl>
@@ -27,6 +45,7 @@ const AppointmentDayField: React.FC<AppointmentDayFieldProps> = ({ setValue, wat
         minDate={new Date()}
         minTime={new Date(new Date().setHours(11, 0, 0, 0))}
         maxTime={new Date(new Date().setHours(20, 0))}
+        filterTime={filterTime}
         customInput={<Input borderColor="teal.400" focusBorderColor="teal.600" />}
       />
       {errors.appointmentDay && <Text data-testid="appointmentDay-error" color="red.500" mt={1}>{errors.appointmentDay.message}</Text>}
