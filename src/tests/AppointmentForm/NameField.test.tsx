68113a2930a43fce5715a725c3e5d006ab522f42
LucasGominho <lcpg@cesar.school>
2024-07-17 02:25:06 -0300

    test: NameField renders and changes input

---
+++ src/tests/AppointmentForm/NameField.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import NameField from '../../components/AppointmentForm/NameField';
+import { useForm } from 'react-hook-form';
+import { FormData } from '../../interfaces/Forms.interfaces';
+
+const Wrapper = () => {
+  const { register, formState: { errors } } = useForm<FormData>();
+  return <NameField register={register} errors={errors} />;
+};
+
+test('renders NameField and changes input', () => {
+  render(<Wrapper />);
+  
+  const nameInput = screen.getByLabelText('Nome:') as HTMLInputElement;
+  expect(nameInput).toBeInTheDocument();
+
+  fireEvent.change(nameInput, { target: { value: 'Lucas Gominho' } });
+  expect(nameInput.value).toBe('Lucas Gominho');
+});
