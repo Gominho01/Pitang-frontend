1c29c57e67cd125e948c4c2305234f4126e2c490
LucasGominho <lcpg@cesar.school>
2024-07-18 16:01:26 -0300

    feat: redirect to valid hour if trying to use invalid hour

--- src/components/AppointmentForm/AppointmentDayField.tsx
+++ src/components/AppointmentForm/AppointmentDayField.tsx
@@ -5,9 +5,11 @@ import 'react-datepicker/dist/react-datepicker.css';
 import { AppointmentDayFieldProps } from '../../interfaces/Forms.interfaces';
 import { removeMilliseconds } from '../../utils/appointmentsUtils';
 import dayjs from 'dayjs';
+import { useModal } from '../../hooks/useModal';
 
 const AppointmentDayField: React.FC<AppointmentDayFieldProps> = ({ setValue, watch, errors }) => {
   const watchAppointmentDay = watch('appointmentDay');
+  const { openModal } = useModal();
 
   const isDisabledTime = (time: Date) => {
     const currentHour = dayjs().hour();
@@ -26,17 +28,39 @@ const AppointmentDayField: React.FC<AppointmentDayFieldProps> = ({ setValue, wat
     return !isDisabledTime(time);
   };
 
+  const handleChange = (date: Date | null) => {
+    if (date) {
+      const selectedDate = dayjs(date);
+      const currentDateTime = dayjs();
+
+      if (selectedDate.hour() < 11 || selectedDate.hour() > 20 ){
+        openModal('Horário inválido', '#FC100D');
+        return;
+      }
+      if (
+        dayjs().isSame(selectedDate, 'day') &&
+        selectedDate.hour() < currentDateTime.hour()
+      ) {
+        const nextValidDate = currentDateTime.hour() >= 20 ? currentDateTime.add(1, 'day').hour(11).minute(0) : currentDateTime.hour(20).minute(0);
+        setValue('appointmentDay', removeMilliseconds(nextValidDate.toDate()));
+        if (selectedDate.hour() === 11) {
+          return;
+        }
+        openModal('Esse horário não está disponível', '#FC100D');
+        return;
+      }
+      setValue('appointmentDay', removeMilliseconds(date));
+      console.log(date)
+    }
+  };
+
   return (
     <FormControl>
       <FormLabel htmlFor="appointmentDay" color="teal.700">Data e Hora do Agendamento:</FormLabel>
       <DatePicker
         id="appointmentDay"
         selected={watchAppointmentDay || null}
-        onChange={(date) => {
-          if (date) {
-            setValue('appointmentDay', removeMilliseconds(date));
-          }
-        }}
+        onChange={handleChange}
         showTimeSelect
         timeFormat="HH:mm"
         timeIntervals={60}
